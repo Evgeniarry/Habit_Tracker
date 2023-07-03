8a04dfe178f846fcc4ffb1a478d8397e274206b7
Evgeniarry <evgeniarry@mail.ru>
2023-07-03 23:22:50 +0300

    Добавьте файлы проекта.

---
+++ Habit_Trecker/Habit_Trecker/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Habit_Trecker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Load();
+        }
+        public void Load()
+        {
+            string s = DateTime.Now.ToString("MMMM yyyy", CultureInfo.GetCultureInfo("ru"));
+            labelDate.Text = s[0].ToString().ToUpper() + s.Substring(1);
+        }
+        
+    }
+}
