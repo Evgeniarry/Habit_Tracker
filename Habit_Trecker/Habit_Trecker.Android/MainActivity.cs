7c941053e2437cfbf66912677bda2d40fb686742
Evgeniarry <evgeniarry@mail.ru>
2023-07-07 03:23:26 +0300

    Добавлен экран загрузки

--- Habit_Trecker/Habit_Trecker.Android/MainActivity.cs
+++ Habit_Trecker/Habit_Trecker.Android/MainActivity.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
@@ -7,7 +6,9 @@ using Android.OS;
 
 namespace Habit_Tracker.Droid
 {
-    [Activity(Label = "Habit_Tracker", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Habit_Tracker", Theme = "@style/MainTheme", MainLauncher = false, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode 
+        | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
