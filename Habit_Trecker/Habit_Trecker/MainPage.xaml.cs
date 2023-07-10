be9d8936d8b503eb7c20bc815e7258c755c5eafe
Evgeniarry <evgeniarry@mail.ru>
2023-07-10 22:56:34 +0300

    Добавлена новая страница

--- Habit_Trecker/Habit_Trecker/MainPage.xaml.cs
+++ Habit_Trecker/Habit_Trecker/MainPage.xaml.cs
@@ -21,6 +21,11 @@ namespace Habit_Tracker
             string s = DateTime.Now.ToString("MMMM yyyy", CultureInfo.GetCultureInfo("ru"));
             labelDate.Text = s[0].ToString().ToUpper() + s.Substring(1);
         }
-        
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddHabit());
+            
+        }
+
     }
 }
